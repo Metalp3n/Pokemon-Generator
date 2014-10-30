656aebce5df0ee669d4526de405c301c54072048
Metalp3n <mike@rapidpurple.com>
2014-10-30 14:01:42 -0400

    Separated class

---
+++ pokemonClass.php
@@ -0,0 +1,41 @@
+<?php
+class pokemon
+{
+	var $nickName;
+	var $key;
+
+function chooseName() //Randomly chooses the Pokemon
+{
+    $name = Array("Charmander", "Pikachu", "Seal", "Bulbasaur");
+    $pName = $name[rand(0, count($name) - 1)];
+	global $key;
+	$key = array_search($pName, $name); 
+	return $pName;
+	
+}
+	
+function displayNickname($customnickName) //Displays the nickname passed to the function
+{
+	$nickName = $customnickName;
+	return $nickName;	
+}
+	
+function chooseType() // Selects the Pokemon type based on the key set in the chooseName() function above
+{
+	global $key;
+	$types = Array("Fire", "Electric", "Water", "Leaf");
+	$t = $types[$key];
+	return $t;
+}
+	
+function getRandomNumber($min, $max) // Selects a random number based on a minimum and maximum range
+{
+    $values = range($min, $max);
+    shuffle($values);
+	$k = array_rand($values);
+	$a = $values[$k];
+	return $a;
+}
+	
+}
+?>
