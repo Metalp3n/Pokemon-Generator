2851940f334db42ab8a4966b8a9adcb8d494a7a1
Michael Boguslavskiy <mike@rapidpurple.com>
2014-10-30 22:06:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ if (!empty($_POST))
 {
 $objPokemon = new pokemon;
 $nickName = $_POST['nickname'];
+$objPokemon->nickName = $nickName;
 echo "You Pulled A <b>" . $objPokemon->chooseName() . "</b><br/>";
-echo "Nickname: <b>" . $objPokemon->displayNickname($nickName) . "</b><br/>";
+echo "Nickname: <b>" . $objPokemon->displayNickname() . "</b><br/>";
 echo "Type: <b>" . $objPokemon->chooseType(2) . "</b><br/>";
 echo "HP: <b>" . $objPokemon->getRandomNumber(80, 180) . "</b><br/>";
 echo "Attack: <b>" . $objPokemon->getRandomNumber(40, 95) . "</b><br/>";
