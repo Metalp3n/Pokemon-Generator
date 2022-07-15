db7ae15d869bd20a31bd0a17743b219e87819616
Metalp3n <michaelbog@gmail.com>
2022-07-14 23:22:42 -0400

    Adding a PokeAPI Implementation to expand on the original Class.

--- pokemonClass.php
+++ pokemonClass.php
@@ -5,6 +5,12 @@ class pokemon
 
 function chooseName() //Randomly chooses the Pokemon
 {
+    //PokeAPI Implementation
+	$pokeAPIurl = 'https://pokeapi.co/api/v2/pokemon/?limit=500';
+	$pokeAPIjson = file_get_contents($url);
+	$pokeAPI = json_decode($json);
+	print_r($pokeAPI);
+
     $name = Array("Charmander", "Pikachu", "Seal", "Bulbasaur");
     $pName = $name[rand(0, count($name) - 1)];
 	global $key;
